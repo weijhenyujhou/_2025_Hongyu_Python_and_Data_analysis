0f2a873108c5ab248e3c7405c4c10b99169403a8
WeiYC <weiyu1213@gmail.com>
2025-03-09 14:14:16 +0800

    運算子導入

--- 0309/數覺運算式.py
+++ 0309/數覺運算式.py
@@ -14,3 +14,28 @@ print(abs(x)) #絕對值
 print(round(x)) # 四捨五入
 print(pow(x,3)) #x 3次方
 print(math.cos(45))
+
+"""
+運算子
+"""
+
+print(math.pi)
+
+#運算子
+a = 10
+b = 6
+# 數學
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+# 比較
+
+# 指定
+print('運算錢未指定A前',a)
+
+a += b
+print('運算後指定A後',a)
+
+
