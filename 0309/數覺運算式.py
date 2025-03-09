4a40c0ae11cb51df0864be70c48b2e9512f0c9ba
WeiYC <weiyu1213@gmail.com>
2025-03-09 13:46:17 +0800

    新增型別轉換

---
+++ 0309/數覺運算式.py
@@ -0,0 +1,15 @@
+#
+import math
+x = 3.6
+y = -34
+# print(round(x))
+# print(abs(x))
+print(max(2,3,1))
+
+print(math.floor(x)) # 無條件捨去
+print(math.ceil(x)) # 無條件進位
+
+
+print(abs(x)) #絕對值
+print(round(x)) # 四捨五入
+print(pow(x,3)) #x 3次方
