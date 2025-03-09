17d5254577c7cdcf7a96eea3f40c81d7696fbf6c
WeiYC <weiyu1213@gmail.com>
2025-03-09 14:58:09 +0800

    小練習

--- 0309/cal.py
+++ 0309/cal.py
@@ -1,5 +1,11 @@
 # 圓徑 2*pi*r
 import math
-r = input('請輸入半徑')
-result = 2* math.pi * float(r)
-print('周長為', result)
+r = float(input('請輸入半徑'))
+result = 2* math.pi * r
+print('周長為',round(result,2))
+
+
+# 面積
+
+result_1 = pow(r,2)*math.pi
+print('面積為', round(result_1,2))
