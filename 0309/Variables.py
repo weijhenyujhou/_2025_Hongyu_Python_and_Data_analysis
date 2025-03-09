b585d69bbd993b784b9a8b4a0186276bd014bdf1
WeiYC <weiyu1213@gmail.com>
2025-03-09 12:08:49 +0800

    新增型別轉換

---
+++ 0309/Variables.py
@@ -0,0 +1,25 @@
+#變數
+#命名規則
+#1. 保留字不能使用
+#2. 變數名稱開頭不能是數字
+
+name='John' #string
+age = 20  #int
+weight = 78.8 # float
+Female = False  #bool
+print(name,age,weight,Female)
+
+#型別轉換
+
+#x = int(input('請輸入第一個數值'))
+#y = int(input('請輸入第二個數值'))
+
+#print(x+y)
+
+a = '100'
+print(int(a))
+print(float(a))
+
+b =3.5
+print(int(b))
+print(str(b))
