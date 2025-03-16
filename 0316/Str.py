c2a876a9319b941804db63f096b874ed9b4f3463
WeiYC <weiyu1213@gmail.com>
2025-03-16 09:47:44 +0800

    小練習

---
+++ 0316/Str.py
@@ -0,0 +1,19 @@
+# %
+'''
+# % + s 表示顯示時後面要插入的數值型態
+%s=str
+%d=int(10進位Decimal）
+%x=16進位
+%o=8進位
+%f=float (Python最初顯示方式）
+'''
+
+
+a = 'Hello %s' # % + s 表示顯示時後面要插入的數值型態 s=str d=int(10進位Decimal） f=float (Python最初顯示方式）
+b = 'Hello %d'
+c = 'Hello %f'
+d = 'Hellot %x'
+print(a % 'John')
+print(b % 123)
+print(c % 123.4)
+print(d % 17)
