05449cf665db79ccba71cc3f40c7ad5b1989ddd1
WeiYC <weiyu1213@gmail.com>
2025-03-16 14:39:09 +0800

    0316_List

--- 0316/List.py
+++ 0316/List.py
@@ -7,7 +7,7 @@ from time import process_time_ns
 #l1 =[]
 #l1 = list()
 
-l1=['apple',123,0.98,True,True,'apple',123]
+l1=['apple',123,0.98,True,True,'banana',123]
 #print(l1[:2])
 
 #取出list資料方式1 for迴圈
@@ -15,3 +15,28 @@ for data in l1:
     print(data)
 
 # while
+# index 方式取出值 不會清空list
+print('=========')
+index = 0
+while index < len(l1):
+    print(l1[index])
+    index +=1
+# pop方式取出值 但是會清空list
+print('========')
+while l1:
+    print(l1.pop(0))
+# print(l1) 列出l1 list
+print('======分隔線======')
+l1=['apple',123,0.98,True,True,'banana',123]
+# len()
+print(len(l1))
+# count()
+print(l1.count('banana'))
+# index()
+print(l1.index('apple'))
+
+
+
+
+
+
