14d8320f8dbdcfed0413c7a789bfb7dfa0115531
WeiYC <weiyu1213@gmail.com>
2025-03-16 10:29:04 +0800

    0317_字串顯示方式

--- 0316/Str.py
+++ 0316/Str.py
@@ -20,7 +20,26 @@ print(b % 123)
 print(c % 123.4)
 print(d % 17)
 
-#最新的顯示方式 字串插補法
+
+e = 'Hello %10s' # % + s 表示顯示時後面要插入的數值型態 s=str d=int(10進位Decimal） f=float (Python最初顯示方式）
+f = 'Hello %d'
+g = 'Hello %.3f'
+h = 'Hello %x'
+
+print(e % 'John')
+#print(b % 123)
+print(g % 123.456789)
+#print(d % 17)
+
+#字串插補法
 # f '顯示文字'+{變數}
 my_name = 999
-print(f'Hello {my_name}')
+print(f"Hello {my_name:*^10}")
+
+
+#Practic
+
+msg = 'Hello %s ! 今日天氣溫度 %.1f'
+
+print(msg %('John',23))
+
