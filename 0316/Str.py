c7bb3cde651bc40208cd8c28801ebcf017f5d9a2
WeiYC <weiyu1213@gmail.com>
2025-03-16 11:24:12 +0800

    0317_字串顯示方式 1.% 2.format() 3.f-string 4.反斜線\ 字元跳脫

--- 0316/Str.py
+++ 0316/Str.py
@@ -38,7 +38,7 @@ print(f"Hello {my_name:*^10}")
 
 
 #Practic
-msg = "1.百分比_顯示_Hello %s ! 今日天氣溫度 %f"
+msg = "1.百分比_顯示_Hello %s ! 今日天氣溫度 %.2f"
 
 
 print(msg %('John',23))
@@ -56,5 +56,13 @@ print(msg_3.format(name ='John',deg=23))
 # f-string
 name = 'John'
 deg =23
+email = "adfg@gmail.com"
 print(f'3.f-string方式 Hello {name:=^10s} ! 今日天氣溫度 {deg:.2f}')
 
+# \ 跳脫字元 用途
+#a = 'Hello "John"'
+#a = "Hello 'John'"
+
+#\n 換行 \t tab 空格定位
+
+print(f'Hello\t{name:=^10s}!\ne-mail:\t{email}')
