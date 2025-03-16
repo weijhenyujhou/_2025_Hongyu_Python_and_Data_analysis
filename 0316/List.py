da9298060cf5a8583a7242e1d558648c08009666
WeiYC <weiyu1213@gmail.com>
2025-03-16 14:24:14 +0800

    0316_List

---
+++ 0316/List.py
@@ -0,0 +1,17 @@
+'''
+List 串列
+'''
+from time import process_time_ns
+
+#宣告一個空間給l1
+#l1 =[]
+#l1 = list()
+
+l1=['apple',123,0.98,True,True,'apple',123]
+#print(l1[:2])
+
+#取出list資料方式1 for迴圈
+for data in l1:
+    print(data)
+
+# while
