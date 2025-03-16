17e7e1d3715c6a55a6d43c8c7bdd4b4365fc79bc
WeiYC <weiyu1213@gmail.com>
2025-03-16 09:53:02 +0800

    0317_字串顯示方式

--- 0316/Str.py
+++ 0316/Str.py
@@ -1,3 +1,4 @@
+#python 顯示變數的方式
 # %
 '''
 # % + s 表示顯示時後面要插入的數值型態
@@ -12,8 +13,14 @@
 a = 'Hello %s' # % + s 表示顯示時後面要插入的數值型態 s=str d=int(10進位Decimal） f=float (Python最初顯示方式）
 b = 'Hello %d'
 c = 'Hello %f'
-d = 'Hellot %x'
+d = 'Hello %x'
+
 print(a % 'John')
 print(b % 123)
 print(c % 123.4)
 print(d % 17)
+
+#最新的顯示方式 字串插補法
+# f '顯示文字'+{變數}
+my_name = 999
+print(f'Hello {my_name}')
