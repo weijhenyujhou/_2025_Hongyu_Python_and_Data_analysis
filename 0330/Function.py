45b6016167b078ebb63bd15459de8f5329430e85
WeiYC <weiyu1213@gmail.com>
2025-03-30 10:39:02 +0800

    0330 函式庫定義 1. 設計一個人員書入秒數到數計時器

--- 0330/Function.py
+++ 0330/Function.py
@@ -4,11 +4,8 @@ def functionName():
 
 回傳值類型
 return 將計算後的值回傳函式
-
+pass
 '''
-    
-
-
 def foo():
     x = 100
     y = 1.2
@@ -28,6 +25,16 @@ print(area(200,2))
 
 def jp_to_tw(Doller,rate):
     return Doller * rate
-
 print(f'不按照韓式變數宣告順序給變數(Rate,Doller)：{jp_to_tw(rate=0.23,Doller=10000)}')
 print(f'按照函式變數宣告順序給變數(Doller, Rate)：{jp_to_tw(10000,0.23)}')
+
+#倒數計時器功能
+s = int(input('請輸入秒數'))
+import time
+def count(end,start=0,):#有預設值時該變數一率放最後面
+    for x in range(end, start,-1): #range(5,1,-1) 由大到小顯示數值
+        print(x)
+        time.sleep(1)
+    print('Finish')
+
+count(s)
