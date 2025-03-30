4f30f2a3a8bd59990d70d0fcf4309fa1adda0c3f
WeiYC <weiyu1213@gmail.com>
2025-03-30 12:32:43 +0800

    0330 網路爬蟲如何使用

--- 0330/Crawler.py
+++ 0330/Crawler.py
@@ -1,6 +1,7 @@
 import urllib.request as req
 
 url = 'https://tw.yahoo.com'
+#如果遇到無法爬蟲的網頁確認 進入網頁程式原始碼底下的網路底下的index 增加header= user-agent資訊
 request =req.Request(url,headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'})
 
 with req.urlopen(request) as response:
