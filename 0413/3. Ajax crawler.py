f17a8263e736722365eba3993f5d1c7480679321
weijhenyujhou <weiyu1213@gmail.com>
2025-04-15 07:49:09 +0000

    0415_

--- 0413/3. Ajax crawler.py
+++ 0413/3. Ajax crawler.py
@@ -67,6 +67,6 @@ from Reuqest import request_get
 #     url = url + str(i+1)
 
 url = ('https://www.kkday.com/zh-tw/category/ajax_product_list?keyword=&currency=TWD&sort=prec&start=0&count=10&destination=&availstartdate=&availenddate=&prodcat=CATEGORY_020%2CCATEGORY_023%2CCATEGORY_022%2CCATEGORY_025%2CCATEGORY_030%2CCATEGORY_028%2CCATEGORY_021%2CCATEGORY_026%2CCATEGORY_029%2CCATEGORY_024%2CCATEGORY_027&ave_score_from=&locations=&time=&glang=&row=10&fprice=*&eprice=*&immediately_use=0&sale_status=0&departure=&arrival=&travel_type=&rewrite=1&csrf_token_name=6febb77a6695c7dba8e9892287bfed1d&page=')
-Urllib_get(url,2)
+#Urllib_get(url,2)
 request_get(url,2)
 
