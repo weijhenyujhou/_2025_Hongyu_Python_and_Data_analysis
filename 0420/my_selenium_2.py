4fce72441557eb1701968210484aba6ec992f9d7
WeiYC <weiyu1213@gmail.com>
2025-04-20 14:17:46 +0800

    0420 Opendata

--- 0420/my_selenium_2.py
+++ 0420/my_selenium_2.py
@@ -7,11 +7,21 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Safari()
-url='https://www.uniqlo.com/tw/zh_TW/c/all_men-outer-casual.html'
+url_tenlogn='https://www.tenlong.com.tw'
+url_momo= 'https://www.momoshop.com.tw/main/Main.jsp'
+url_nike=''
 time.sleep(3)
-driver.get(url)
+driver.get(url_momo)
 
-
-titles = driver.find_elements(By.CLASS_NAME,'ec-font-sub-title mt-4')
+#
+titles = driver.find_elements(By.CSS_SELECTOR,'#menuList a') # 樹狀
+#titles = driver.find_elements(By.CSS_SELECTOR,'.keywords-list a') # 樹狀
 for title in titles:
-    print(title)
+    print(title.text)
+#
+# kw = driver.find_element(By.ID,'keyword')
+# kw.send_keys('python')
+# time.sleep(5)
+
+# print(titles.text)
+
