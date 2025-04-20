746e353ee42dfed5047a9e0a5050d303a66af7fd
WeiYC <weiyu1213@gmail.com>
2025-04-20 12:03:16 +0800

    0420 Opendata

---
+++ 0420/my_selenium.py
@@ -0,0 +1,16 @@
+from charset_normalizer.cli import cli_detect
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Chrome()
+url ='https://www.books.com.tw/' # 博客來
+driver.get(url)
+time.sleep(2)
+# driver.save_screenshot('screenshot.jpg')
+search = driver.find_element(By.CLASS_NAME,'search_key')
+search.send_keys('python')
+# search.send_keys('餐')
+# time.sleep(3)
+search.send_keys(Keys.ENTER)
+time.sleep(10)
