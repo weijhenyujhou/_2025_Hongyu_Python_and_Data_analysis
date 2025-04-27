de596c10e83a1d77c552d2aebee9288993ef3bb2
weiyu@macbookpro <weiyu1213@gmail.com>
2025-04-27 16:32:50 +0800

    0427 Matplotlib

--- 0427/2.Matplotlib.py
+++ 0427/2.Matplotlib.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
+from matplotlib.lines import lineStyles
 
+list_x = [15,26,38,15,22,30]
+list_y = [1,3,5,7,9,11]
 
-listx = [15,26,38,15,22,30]
-listy = [1,3,5,7,8,10]
+plt.plot(list_x,list_y,marker="s",color='#00bfff',linestyle=':', linewidth=2,markersize=10)
+plt.title('test') #表格主旨
+plt.xlabel("degree") #x軸標籤
+plt.ylabel("day") #y軸的標籤
 
-plt.plot(listx,listy,)
 plt.show()
